74c3580a190fd37023b1b3376ed06279617892a8
Oleg Stakhiv <veishen19@gmail.com>
2017-04-18 18:10:12 +0300

    magic methods of php: sleep and wakeup

---
+++ sleep_wakeup.php
@@ -0,0 +1,31 @@
+<?php
+
+    error_reporting(E_ALL);
+
+    class City {
+        public $name;
+        public $population;
+
+        public function __construct($name, $population){
+            $this -> name = $name;
+            $this -> population = $population;
+        }
+
+        /** Returns the properties that should be serialized and invoke before serialization of object */
+        public function __sleep(){
+            return ['name', 'population'];
+        }
+
+        /** Invoke before unserialization of object */
+        public function __wakeup(){
+            echo 'Unserialization';
+        }
+    }
+
+    $London = new City('London', 10000000);
+
+    $serialized = serialize($London);
+
+    echo "<pre>";
+    var_dump(unserialize($serialized));
+    echo "</pre>";
