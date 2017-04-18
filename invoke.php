24a5ad9a37417c064cbcb9b7ec0db75a7924aacd
Oleg Stakhiv <veishen19@gmail.com>
2017-04-18 18:35:22 +0300

    invoke magic method

---
+++ invoke.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Science {
+
+        public $current_exploring_technology = 'Pottery';
+        public $explored_technologies = [];
+
+        /** The invoke method is called when a script tries to call an object as a function */
+        public function __invoke(){
+            echo $this -> current_exploring_technology;
+        }
+
+    }
+
+    $Science = new Science();
+
+    $Science();
